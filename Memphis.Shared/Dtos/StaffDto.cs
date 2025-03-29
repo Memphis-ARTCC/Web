9ab9040a393f804d1c8b93606383bdc2741ad6ce
fixterjake <fixterjake@gmail.com>
2025-03-29 10:53:11 -0400

    staff page

--- Memphis.Shared/Dtos/StaffDto.cs
+++ Memphis.Shared/Dtos/StaffDto.cs
@@ -1,21 +1,38 @@
 ﻿namespace Memphis.Shared.Dtos;
 
-public class StaffDto
+
+public class StaffResponseDto
 {
-    public RosterUserDto? ATM { get; set; }
-    public RosterUserDto? DATM { get; set; }
-    public RosterUserDto? TA { get; set; }
-    public RosterUserDto? ATA { get; set; }
-    public RosterUserDto? WM { get; set; }
-    public RosterUserDto? AWM { get; set; }
-    public RosterUserDto? WebTeam { get; set; }
-    public RosterUserDto? EC { get; set; }
-    public RosterUserDto? AEC { get; set; }
-    public RosterUserDto? EventsTeam { get; set; }
-    public RosterUserDto? FE { get; set; }
-    public RosterUserDto? AFE { get; set; }
-    public RosterUserDto? FacilitiesTeam { get; set; }
-    public RosterUserDto? SocialMediaTeam { get; set; }
-    public RosterUserDto? INS { get; set; }
-    public RosterUserDto? MTR { get; set; }
+    public string? Atm { get; set; }
+    public string? AtmEmail { get; set; }
+    public string? Datm { get; set; }
+    public string? DatmEmail { get; set; }
+    public string? Ta { get; set; }
+    public string? TaEmail { get; set; }
+    public string? Ata { get; set; }
+    public string? AtaEmail { get; set; }
+    public string? Wm { get; set; }
+    public string? WmEmail { get; set; }
+    public string? Awm { get; set; }
+    public string? AwmEmail { get; set; }
+    public IList<string>? WebTeam { get; set; }
+    public string? WebTeamEmail { get; set; }
+    public string? Ec { get; set; }
+    public string? EcEmail { get; set; }
+    public string? Aec { get; set; }
+    public string? AecEmail { get; set; }
+    public IList<string>? EventsTeam { get; set; }
+    public string? EventsTeamEmail { get; set; }
+    public string? Fe { get; set; }
+    public string? FeEmail { get; set; }
+    public string? Afe { get; set; }
+    public string? AfeEmail { get; set; }
+    public IList<string>? FacilitiesTeam { get; set; }
+    public string? FacilitiesTeamEmail { get; set; }
+    public IList<string>? SocialMediaTeam { get; set; }
+    public string? SocialMediaTeamEmail { get; set; }
+    public IList<string>? Ins { get; set; }
+    public string? InsEmail { get; set; }
+    public IList<string>? Mtr { get; set; }
+    public string? MtrEmail { get; set; }
 }
