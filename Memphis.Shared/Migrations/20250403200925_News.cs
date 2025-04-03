227f6ad92e0c0639c2d43d40db2350384b53fc84
fixterjake <fixterjake@gmail.com>
2025-04-03 16:21:13 -0400

    home page and file stuff

---
+++ Memphis.Shared/Migrations/20250403200925_News.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Memphis.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class News : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_News_Users_AuthorId",
+                table: "News");
+
+            migrationBuilder.DropIndex(
+                name: "IX_News_AuthorId",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "News");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "News",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "News");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "News",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_AuthorId",
+                table: "News",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_News_Users_AuthorId",
+                table: "News",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
