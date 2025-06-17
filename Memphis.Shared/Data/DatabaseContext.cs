6f230f41cfe9a9c3871ffd5336e071c930dcef98
fixterjake <fixterjake@gmail.com>
2025-06-17 11:41:26 -0400

    need migrations

--- Memphis.Shared/Data/DatabaseContext.cs
+++ Memphis.Shared/Data/DatabaseContext.cs
@@ -35,4 +35,122 @@ public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbCont
     public required DbSet<User> Users { get; set; }
     public required DbSet<VisitingApplication> VisitingApplications { get; set; }
     public required DbSet<WebsiteLog> WebsiteLogs { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Seed roles
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 1,
+            Name = "Air Traffic Manager",
+            NameShort = "ATM",
+            Email = "atm@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 2,
+            Name = "Deputy Air Traffic Manager",
+            NameShort = "DATM",
+            Email = "datm@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 3,
+            Name = "Training Administrator",
+            NameShort = "TA",
+            Email = "ta@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 4,
+            Name = "Assistant Training Administrator",
+            NameShort = "ATA",
+            Email = "ata@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 5,
+            Name = "Webmaster",
+            NameShort = "WM",
+            Email = "wm@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 6,
+            Name = "Assistant Webmaster",
+            NameShort = "AWM",
+            Email = "awm@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 7,
+            Name = "Events Coordinator",
+            NameShort = "EC",
+            Email = "ec@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 8,
+            Name = "Assistant Events Coordinator",
+            NameShort = "AEC",
+            Email = "aec@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 9,
+            Name = "Facility Engineer",
+            NameShort = "FE",
+            Email = "fe@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 10,
+            Name = "Assistant Facility Engineer",
+            NameShort = "AFE",
+            Email = "afe@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 11,
+            Name = "Web Team",
+            NameShort = "WEB",
+            Email = "web@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 12,
+            Name = "Events Team",
+            NameShort = "EVENTS",
+            Email = "events@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 13,
+            Name = "Facilities Team",
+            NameShort = "FACILITIES",
+            Email = "facilities@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 14,
+            Name = "Social Media Team",
+            NameShort = "SOCIAL",
+            Email = "socialmedia@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 15,
+            Name = "Instructor",
+            NameShort = "INS",
+            Email = "instructors@memphisartcc.com"
+        });
+        modelBuilder.Entity<Role>().HasData(new Role
+        {
+            Id = 16,
+            Name = "Mentor",
+            NameShort = "MTR",
+            Email = "mentors@memphisartcc.com"
+        });
+    }
 }
